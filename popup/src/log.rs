a1df34f68cd60728f0f1254fe8fc5018ec38bc15
THELOSTSOUL <1095533751@qq.com>
2024-10-05 22:30:29 +0800

    收藏日志

--- popup/src/log.rs
+++ popup/src/log.rs
@@ -1,3 +1,4 @@
+use leptos::ev::Event;
 use leptos::*;
 use module::http::{Request, Response};
 use serde::ser::Serializer as _;
@@ -14,21 +15,46 @@ use crate::browser::{browser, get_local};
 
 const INDEXES: &str = "indexes";
 
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct LogIndexItem {
+    pub id: Uuid,
+    pub method: String,
+    pub uri: String,
+    #[serde(with = "time::serde::timestamp")]
+    pub done_date: OffsetDateTime,
+    #[serde(default)]
+    pub star: bool,
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct LogContent {
+    pub request: Request,
+    pub response: Response,
+}
+
 #[component]
 pub fn LogDrawer(
     indexes: RwSignal<Vec<LogIndexItem>>,
     #[prop(into)] on_select: Callback<LogContent>,
 ) -> impl IntoView {
-    let load_action = create_action(|indexes: &RwSignal<Vec<LogIndexItem>>| load_index(*indexes));
-    load_action.dispatch(indexes);
+    let load_index = create_action(|indexes: &RwSignal<Vec<LogIndexItem>>| load_index(*indexes));
+    load_index.dispatch(indexes);
 
     let get_log = create_action(move |id: &String| {
         let id = id.clone();
         async move {
-            let content = get_log(id).await;
+            let content = get_log(&id).await;
             if let Ok(content) = content {
                 on_select.call(content);
             }
+        }
+    });
+
+    let star = create_action(|index_param: &(RwSignal<Vec<LogIndexItem>>, String)| {
+        let indexes = index_param.0;
+        let id = index_param.1.clone();
+        async move {
+            let _ = star(indexes, &id).await.inspect(|e| error!("{e:?}"));
         }
     });
 
@@ -66,6 +92,12 @@ pub fn LogDrawer(
                                             <p class="break-all">{index.uri}</p>
                                         </div>
                                         <div class="card-actions justify-end">
+                                            <StarButton
+                                                checked=index.star
+                                                on_change=move |_| {
+                                                    star.dispatch((indexes, index.id.to_string()))
+                                                }
+                                            />
                                             <div
                                                 class="badge badge-primary"
                                                 on:click=move |_| get_log.dispatch(index.id.to_string())
@@ -85,19 +117,41 @@ pub fn LogDrawer(
     .into_view()
 }
 
-#[derive(Debug, Clone, Serialize, Deserialize)]
-pub struct LogIndexItem {
-    pub id: Uuid,
-    pub method: String,
-    pub uri: String,
-    #[serde(with = "time::serde::timestamp")]
-    pub done_date: OffsetDateTime,
-}
+#[component]
+pub fn StarButton(checked: bool, on_change: impl FnMut(Event) + 'static) -> impl IntoView {
+    view! {
+        <label class="swap swap-rotate">
+            <input type="checkbox" checked=checked on:change=on_change />
 
-#[derive(Debug, Clone, Serialize, Deserialize)]
-pub struct LogContent {
-    pub request: Request,
-    pub response: Response,
+            <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke-width="1.5"
+                stroke="currentColor"
+                class="swap-off h-5 w-5"
+            >
+                <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    d="M11.48 3.499a.562.562 0 0 1 1.04 0l2.125 5.111a.563.563 0 0 0 .475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 0 0-.182.557l1.285 5.385a.562.562 0 0 1-.84.61l-4.725-2.885a.562.562 0 0 0-.586 0L6.982 20.54a.562.562 0 0 1-.84-.61l1.285-5.386a.562.562 0 0 0-.182-.557l-4.204-3.602a.562.562 0 0 1 .321-.988l5.518-.442a.563.563 0 0 0 .475-.345L11.48 3.5Z"
+                />
+            </svg>
+
+            <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 24 24"
+                fill="currentColor"
+                class="swap-on h-5 w-5 fill-current"
+            >
+                <path
+                    fill-rule="evenodd"
+                    d="M10.788 3.21c.448-1.077 1.976-1.077 2.424 0l2.082 5.006 5.404.434c1.164.093 1.636 1.545.749 2.305l-4.117 3.527 1.257 5.273c.271 1.136-.964 2.033-1.96 1.425L12 18.354 7.373 21.18c-.996.608-2.231-.29-1.96-1.425l1.257-5.273-4.117-3.527c-.887-.76-.415-2.212.749-2.305l5.404-.434 2.082-5.005Z"
+                    clip-rule="evenodd"
+                />
+            </svg>
+        </label>
+    }
 }
 
 async fn load_index(indexes: RwSignal<Vec<LogIndexItem>>) {
@@ -108,8 +162,8 @@ async fn load_index(indexes: RwSignal<Vec<LogIndexItem>>) {
     indexes.set(index_items);
 }
 
-async fn get_log(id: String) -> Result<LogContent, JsValue> {
-    get_local(&id).await
+async fn get_log(id: &str) -> Result<LogContent, JsValue> {
+    get_local(id).await
 }
 
 pub async fn save_log(
@@ -123,9 +177,19 @@ pub async fn save_log(
         method: request.method.clone(),
         uri: request.uri.clone(),
         done_date: response.done_date,
+        star: false,
     };
     let content = LogContent { request, response };
-    indexes.update(|i| i.insert(0, index));
+    indexes.update(|indexes| {
+        let mut i = indexes.len();
+        for (ii, indexed) in indexes.iter().enumerate() {
+            if !indexed.star {
+                i = ii;
+                break;
+            }
+        }
+        indexes.insert(i, index);
+    });
 
     let serializer = Serializer::json_compatible();
     let index_key = JsValue::from_str(INDEXES);
@@ -135,6 +199,44 @@ pub async fn save_log(
     let items = Map::new()
         .set(&index_key, &index_value)
         .set(&content_key, &content_value);
+    let items = Object::from_entries(&items);
+    if let Ok(ref items) = items {
+        browser().storage().local().set(items).await?;
+    }
+
+    Ok(())
+}
+
+pub async fn star(indexes: RwSignal<Vec<LogIndexItem>>, id: &str) -> Result<(), JsValue> {
+    indexes.update(|indexes| {
+        let i = indexes
+            .iter()
+            .enumerate()
+            .filter_map(|(i, index)| {
+                if id == index.id.to_string() {
+                    Some(i)
+                } else {
+                    None
+                }
+            })
+            .next();
+
+        if let Some(i) = i {
+            let mut index = indexes.remove(i);
+            if index.star {
+                index.star = false;
+                indexes.push(index);
+            } else {
+                index.star = true;
+                indexes.insert(0, index);
+            }
+        }
+    });
+
+    let serializer = Serializer::json_compatible();
+    let index_key = JsValue::from_str(INDEXES);
+    let index_value = serializer.serialize_some(&indexes.get())?;
+    let items = Map::new().set(&index_key, &index_value);
     let items = Object::from_entries(&items);
     if let Ok(ref items) = items {
         browser().storage().local().set(items).await?;
